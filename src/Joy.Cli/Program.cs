1f3d2cc7b159aa66e93cdd175c9726379e96104d
Andrea Saltarello <andysal@users.noreply.github.com>
2018-10-10 13:30:16 +0200

    First commit
    Project structure created

---
+++ src/Joy.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Joy.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
